9c9fb5a0b73b770f0b2b52211fbc8abc7f276967
jiayi797 <jiayi797@163.com>
2017-05-03 11:36:00 +0800

    fixed_run

--- run.py
+++ run.py
@@ -15,13 +15,92 @@ ProductFile = "JData_Product.csv"
 UserFile = "JData_User.csv"
 ActionAllFile = "JData_Action_All.csv"
 
-def online():
-    train_start_date_str = '2016-04-01'
-    train_end_date_str = '2016-04-10'
-    label_start_date_str = '2016-04-11'
-    label_end_date_str = '2016-04-15'
-    predict_start_date_str = '2016-04-05'
-    predict_end_date_str = '2016-04-15'
+# def online():
+#     train_start_date_str = '2016-04-01'
+#     train_end_date_str = '2016-04-10'
+#     label_start_date_str = '2016-04-11'
+#     label_end_date_str = '2016-04-15'
+#     predict_start_date_str = '2016-04-05'
+#     predict_end_date_str = '2016-04-15'
+#
+#     train_start_datetime = pd.to_datetime(train_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
+#     train_end_datetime = pd.to_datetime(train_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
+#     label_start_datetime = pd.to_datetime(label_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
+#     label_end_datetime = pd.to_datetime(label_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
+#     predict_start_datetime = pd.to_datetime(predict_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
+#     predict_end_datetime = pd.to_datetime(predict_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
+#
+#     xgb_preds_file = 'xgb_preds_' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
+#     feature_score_file = 'feature_score' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
+#     predict_real_label_file = 'real_label_' + label_start_date_str + 'to' + label_end_date_str + '.csv'
+#     sku_fea_file = 'sku_fea_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
+#     user_fea_file = 'user_fea_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
+#     train_set_file = 'train_set_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
+#     test_set_file = 'test_set_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
+#     submit_file = 'submit_0502.csv'
+#
+#     sku_fea = gen_sku_fea(train_start_datetime, train_end_datetime)
+#     sku_fea.to_csv(FilePath + sku_fea_file, index=False)
+#     print '商品特征生成完毕'
+#
+#     user_fea = gen_user_fea(train_start_datetime, train_end_datetime)
+#     user_fea.to_csv(FilePath + user_fea_file, index=False)
+#     print '用户特征生成完毕'
+#
+#     user_sku_pair_train, train_set, labels = make_train_set(sku_fea, user_fea, train_start_datetime, train_end_datetime,
+#                                                             label_start_datetime, label_end_datetime)
+#     train_data = pd.concat([user_sku_pair_train, train_set, labels], axis=1)
+#     train_data.to_csv(FilePath + train_set_file, index=False)
+#     print '训练集生成完毕'
+#
+#     user_sku_pair_predict, predict_set = make_test_set(sku_fea, user_fea, predict_start_datetime, predict_end_datetime)
+#     test_data = pd.concat([user_sku_pair_predict, predict_set], axis=1)
+#     test_data.to_csv(FilePath + test_set_file, index=False)
+#     print '预测集生成完毕'
+#
+#     xgb_preds, feature_score = train_and_predict(train_set, labels, user_sku_pair_predict, predict_set)
+#     xgb_preds.to_csv(FilePath + xgb_preds_file, index=False)
+#     print '预测完毕'
+#
+#     fs = []
+#     for (key, value) in feature_score:
+#         fs.append("{0},{1}\n".format(key, value))
+#     with open(FilePath + feature_score_file, 'w') as f:
+#         f.writelines("feature,score\n")
+#         f.writelines(fs)
+#
+#     submitcsv = submit(xgb_preds, 0.8)
+#     submitcsv.to_csv(FilePath+submit_file, index=False)
+
+def run_all(ifonline):
+    flag=False
+    train_start_date_str = ''
+    train_end_date_str = ''
+    label_start_date_str = ''
+    label_end_date_str = ''
+    predict_start_date_str = ''
+    predict_end_date_str = ''
+    predict_label_start_date_str = ''
+    predict_label_end_date_str = ''
+    if ifonline=='online':
+        flag=True
+        train_start_date_str = '2016-04-01'
+        train_end_date_str = '2016-04-10'
+        label_start_date_str = '2016-04-11'
+        label_end_date_str = '2016-04-15'
+        predict_start_date_str = '2016-04-05'
+        predict_end_date_str = '2016-04-15'
+    elif ifonline=='offline':
+        flag=False
+        train_start_date_str = '2016-03-27'
+        train_end_date_str = '2016-04-05'
+        label_start_date_str = '2016-04-06'
+        label_end_date_str = '2016-04-10'
+        predict_start_date_str = '2016-04-01'
+        predict_end_date_str = '2016-04-15'
+        predict_label_start_date_str = '2016-04-10'
+        predict_label_end_date_str = '2016-04-15'
+    print ifonline
 
     train_start_datetime = pd.to_datetime(train_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
     train_end_datetime = pd.to_datetime(train_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
@@ -30,66 +109,6 @@ def online():
     predict_start_datetime = pd.to_datetime(predict_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
     predict_end_datetime = pd.to_datetime(predict_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
 
-    xgb_preds_file = 'xgb_preds_' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
-    feature_score_file = 'feature_score' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
-    predict_real_label_file = 'real_label_' + label_start_date_str + 'to' + label_end_date_str + '.csv'
-    sku_fea_file = 'sku_fea_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
-    user_fea_file = 'user_fea_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
-    train_set_file = 'train_set_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
-    test_set_file = 'test_set_' + train_start_date_str + 'to' + train_end_date_str + '.csv'
-    submit_file = 'submit_0502.csv'
-
-    sku_fea = gen_sku_fea(train_start_datetime, train_end_datetime)
-    sku_fea.to_csv(FilePath + sku_fea_file, index=False)
-    print '商品特征生成完毕'
-
-    user_fea = gen_user_fea(train_start_datetime, train_end_datetime)
-    user_fea.to_csv(FilePath + user_fea_file, index=False)
-    print '用户特征生成完毕'
-
-    user_sku_pair_train, train_set, labels = make_train_set(sku_fea, user_fea, train_start_datetime, train_end_datetime,
-                                                            label_start_datetime, label_end_datetime)
-    train_data = pd.concat([user_sku_pair_train, train_set, labels], axis=1)
-    train_data.to_csv(FilePath + train_set_file, index=False)
-    print '训练集生成完毕'
-
-    user_sku_pair_predict, predict_set = make_test_set(sku_fea, user_fea, predict_start_datetime, predict_end_datetime)
-    test_data = pd.concat([user_sku_pair_predict, predict_set], axis=1)
-    test_data.to_csv(FilePath + test_set_file, index=False)
-    print '预测集生成完毕'
-
-    xgb_preds, feature_score = train_and_predict(train_set, labels, user_sku_pair_predict, predict_set)
-    xgb_preds.to_csv(FilePath + xgb_preds_file, index=False)
-    print '预测完毕'
-
-    fs = []
-    for (key, value) in feature_score:
-        fs.append("{0},{1}\n".format(key, value))
-    with open(FilePath + feature_score_file, 'w') as f:
-        f.writelines("feature,score\n")
-        f.writelines(fs)
-
-    submitcsv = submit(xgb_preds, 0.8)
-    submitcsv.to_csv(FilePath+submit_file, index=False)
-
-def offline():
-    train_start_date_str = '2016-03-27'
-    train_end_date_str = '2016-04-05'
-    label_start_date_str = '2016-04-06'
-    label_end_date_str = '2016-04-10'
-    predict_start_date_str = '2016-04-01'
-    predict_end_date_str = '2016-04-15'
-    predict_label_start_date_str = '2016-04-10'
-    predict_label_end_date_str = '2016-04-15'
-
-    train_start_datetime = pd.to_datetime(train_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
-    train_end_datetime = pd.to_datetime(train_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
-    label_start_datetime = pd.to_datetime(label_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
-    label_end_datetime = pd.to_datetime(label_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
-    predict_start_datetime = pd.to_datetime(predict_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
-    predict_end_datetime = pd.to_datetime(predict_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
-    predict_label_start_datetime = pd.to_datetime(predict_label_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
-    predict_label_end_datetime = pd.to_datetime(predict_label_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
 
     xgb_preds_file = 'xgb_preds_' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
     #feature_score_file = 'feature_score' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
@@ -98,10 +117,12 @@ def offline():
     user_fea_file = 'user_fea_'+train_start_date_str+'to'+train_end_date_str+'.csv'
     train_set_file = 'train_set_'+train_start_date_str+'to'+train_end_date_str+'.csv'
     predict_set_file = 'predict_set_' + predict_start_date_str + 'to' + predict_end_date_str + '.csv'
-
-    predict_real_label = get_labels(predict_label_start_datetime, predict_label_end_datetime)
-    predict_real_label.to_csv(FilePath+predict_real_label_file, index=False)
-    print 'predict_reak_label 线下真实label生成完毕'
+    if not flag:
+        predict_label_start_datetime = pd.to_datetime(predict_label_start_date_str + ' 00:00:00', format='%Y-%m-%d %H:%M:%S')
+        predict_label_end_datetime = pd.to_datetime(predict_label_end_date_str + ' 23:59:59', format='%Y-%m-%d %H:%M:%S')
+        predict_real_label = get_labels(predict_label_start_datetime, predict_label_end_datetime)
+        predict_real_label.to_csv(FilePath+predict_real_label_file, index=False)
+        print 'predict_reak_label 线下真实label生成完毕'
 
     sku_fea = gen_sku_fea(train_start_datetime,train_end_datetime)
     sku_fea.to_csv(FilePath+sku_fea_file, index=False)
@@ -131,13 +152,19 @@ def offline():
     xgb_preds.to_csv(FilePath+xgb_preds_file,index=False)
     print '预测完毕'
 
-
-    i = 0.0
-    while i < 1:
-        print i,'score,F11,F12'
-        result = submit(xgb_preds, i)
-        cal_F1(result, predict_real_label)
-        i += 0.1
+    if not flag:
+        i = 0.0
+        while i < 1:
+            print i,'score,F11,F12'
+            result = submit(xgb_preds, i)
+            cal_F1(result, predict_real_label)
+            if i<0.4:
+                i += 0.1
+            else:
+                i += 00.1
+    else:
+        submitcsv = submit(xgb_preds, 0.4)
+        submitcsv.to_csv(FilePath + submit_file, index=False)
 
 if __name__ == '__main__':
-    offline()
+    run_all('offline')
